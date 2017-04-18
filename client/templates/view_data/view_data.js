b92c6f3c358dcb4f00c1af14cbcdb90e339f745c
wannabeCitizen <doorsofskirpan@gmail.com>
2017-04-17 19:14:35 -0600

    ready to make grouped bar charts. pie charts working!

--- client/templates/view_data/view_data.js
+++ client/templates/view_data/view_data.js
@@ -2,6 +2,39 @@
 * JS file for view_data.js
 */
 
+//Generate classroom buttons immediately
+Template.viewData.rendered = function() {
+  var envs = Environments.find({}).fetch();
+  console.log(envs);
+  if ($.isEmptyObject(envs)) {
+    $('.env-selection').append('<h2 class="subtitle is-2" style="color: red;">You must create classroom data before doing an analysis is possible.</h2>')
+  } else {
+
+    var classButtons = $('.env-selection');
+    for (env in envs) {
+      name = envs[env]['envName'];
+      id = envs[env]['_id'];
+
+      var button = $('<button>', {
+        class: "button is-medium classroom-selection",
+        text: name,
+        data_id: id
+      }).appendTo(classButtons);
+
+      button.click(function (e) {
+          e.preventDefault();
+
+          $(this).siblings().removeClass('chosen');
+          if ( $(this).hasClass('chosen') ){
+            $(this).removeClass('chosen');
+          } else {
+            $(this).addClass('chosen');
+          }
+        });
+    }
+  }
+}
+
 Template.viewData.helpers({
    environment: function() {
       return Environments.find({}, {sort: {submitted: -1}});
@@ -20,213 +53,113 @@ Template.viewData.helpers({
    },
 });
 
+
+
 Template.viewData.events({
-   'click #graph_button': function(e) {
-      //clear the graph section before adding new ones
-      $("#graph_container").children().each(function (){
-         this.remove()
-      });
 
-      //lookup tables for the subject and sequence codes
-      var subject_info = {
-         "subjAge":["0-10","10-15","15-20","20-25","Unknown"],
-         "subjGender":["Male","Female","Other","Unknown"],
-         "subjRace":["American Indian or Alaska Native","Asian","Black or African American","Native Hawaiin or Other Pacific Islander","White","Hispanic or Latino","Unknown"]
-      };
+  'click .reset-button': function (e) {
+    location.reload()  
+  },
 
-      var sequence_info = {
-         "wcdType":["Math","Non-Math","Unknown"],
-         "solicitationMethod":["Called On","Not Called On","Unknown"],
-         "waitTime":["Less than 3 seconds","3 or more seconds","N/A"],
-         "lengthOfTalk":["1-4 Words","5-20 Words","21 or more Words","Unknown"],
-         "studentTalk":["How","What","Why","Other","Unknown"],
-         "teacherSolicitation":["How","What","Why","Other"],
-         "explicitEvaluation":["Yes","No","Unknown"]
-      };
+  'click .generate-button': function (e) {
+    // Get classroom, obs, and all params.
+    obsIds = [];
+    dParams = [];
+    sParams = [];
+    envId = $('.env-selection .chosen').attr('data_id');
+    $('.obs-selection .chosen').each(function () { obsIds.push($(this).attr('data_id')) });
+    $('.dparam-selection .chosen').each(function () { dParams.push($(this).attr('data_id')) });
+    $('.sparam-selection .chosen').each(function () { sParams.push($(this).attr('data_id')) });
 
-      //get values from dropdown menus
-      var options = {
-         "environment":$('#selectEnvironment').val(),
-         "student_char":$('#studentCharSelect').val(),
-         "sequence_val":$('#sequenceCharSelect').val()
-      };
+    // Start generating graphs
+    makeDemGraphs(envId, dParams);
+    makeContributionGraphs(obsIds, dParams, sParams);
 
-      //fetch a specific enviornment
-      if(options["environment"]!="all")
-      {
-         var environment=Environments.find({"envName":options["environment"]}).fetch();
-         var environment_id=environment[0]["_id"];
-         var sequences=Sequences.find({"envId":environment_id}).fetch();
-         var subjects=Subjects.find({"envId":environment_id}).fetch();
-      }
-      // fetch all enviornments
-      else
-      {
-         var sequences=Sequences.find().fetch();
-         var subjects=Subjects.find().fetch();
-      }
+    $('.option-select').css('display', 'none');
+    $('.report-body').css('visibility', 'visible');
+  },
+   
+  'click .classroom-selection': function(e) {
+    envId = $(e.target).attr('data_id');
+    obs = Observations.find({"envId": envId}).fetch();
+    dparams = SubjectParameters.findOne({'children.envId': envId});
+    sparams = SequenceParameters.findOne({'children.envId': envId});
 
-      //grab array of sequence and subject info for easier refrencing
-      var sequence_props = sequence_info[options["sequence_val"]];
-      var subject_props = subject_info[options["student_char"]];
+    $(".obs-selection").empty();
+    $(".dparam-selection").empty();
+    $(".sparam-selection").empty();
 
-      //loop through every sequence and grab the selected sequence value and the subject id
-      //then use the subject ID to look up the selected students characteristic
-      var sequence_data=[]
-      for(i=0;i<sequences.length;i++)
-      {
-         temp={};
-         temp_subj_id=sequences[i]["subjId"];
-         temp.subj_data=((Subjects.find({"_id":temp_subj_id}).fetch())[0])[options["student_char"]];
-         temp.seq_data=sequences[i][options["sequence_val"]];
-         sequence_data.push(temp);
-      }
+    var obsButtons = $('.obs-selection');
+    for (ob in obs) {
+      name = obs[ob]['name'];
+      id = obs[ob]['_id'];
 
-      function graph_standard(options)
-      {
-         //parse and format the data for chartist
-         var graph_data=[];
-         for(i=0;i<subject_info[options["student_char"]].length;i++)
-         {
-            var temp_data={"name":subject_info[options["student_char"]][i], "data":[]};
-            for(j=0;j<sequence_info[options["sequence_val"]].length;j++)
-            {
-               temp_data["data"].push(0);
-            }
-            graph_data.push(temp_data);
-         }
-         for(i=0;i<sequence_data.length;i++)
-         {
-            var x = sequence_data[i]["seq_data"];
-            var z = sequence_data[i]["subj_data"];
-            graph_data[z]["data"][x]++;
-         }
-         var data = {
-           labels: sequence_info[options["sequence_val"]],
-           series: graph_data
-         };
+      var button = $('<button>', {
+        class: "button is-medium option-selectors",
+        text: name,
+        data_id: id
+      }).appendTo(obsButtons);
 
-         var options = {
-           seriesBarDistance: 10
-         }*
-         new Chartist.Bar('#ct-chart-standard', data, {
-               plugins: [
-                   Chartist.plugins.legend()
-               ]
-           });
-         }
+      button.click(function (e) {
+          e.preventDefault();
 
-      function graph_demographics(options, subjects)
-      {
-         //loop through and tally the number of students in each category of the selected characteristic
-         var temp_graph_data=[];
-         for(i=0;i<subject_info[options["student_char"]].length;i++)
-         {
-            temp_graph_data.push(0);
-         }
-         for(i=0;i<subjects.length;i++)
-         {
-            temp_graph_data[subjects[i][[options["student_char"]]]]++;
-         }
-         graph_data=[];
-         graph_data.push(temp_graph_data);
-         var data = {
-            labels: subject_info[options["student_char"]],
-            series: graph_data
-         };
-         var options = {
-           seriesBarDistance: 10
-         }*
-         new Chartist.Bar('#ct-chart-demographic', data);
-      }
+          if ( $(this).hasClass('chosen') ){
+            $(this).removeClass('chosen');
+          } else {
+            $(this).addClass('chosen');
+          }
+        });
+    }
 
-      function graph_scaled(options, subjects)
-      {
-         //get demographic tally
-         var demographic_data=[];
-         for(i=0;i<subject_info[options["student_char"]].length;i++)
-         {
-            demographic_data.push(0);
-         }
-         for(i=0;i<subjects.length;i++)
-         {
-            demographic_data[subjects[i][[options["student_char"]]]]++;
-         }
-         var num_of_students=0;
-         var equity_factor = []
-         for(i=0;i<demographic_data.length;i++)
-         {
-            num_of_students+=demographic_data[i];
-         }
-         for(i=0;i<demographic_data.length;i++)
-         {
-            if(num_of_students==0){
-               equity_factor[i]=0;
-            }
-            else
-            {
-            equity_factor[i]=demographic_data[i]/num_of_students;
-            }
-         }
-         var graph_data=[];
-         for(i=0;i<subject_info[options["student_char"]].length;i++)
-         {
-            var temp_data={"name":subject_info[options["student_char"]][i], "data":[]};
-            for(j=0;j<sequence_info[options["sequence_val"]].length;j++)
-            {
-               temp_data["data"].push(0);
-            }
-            graph_data.push(temp_data);
-         }
-         for(i=0;i<sequence_data.length;i++)
-         {
-            var x = sequence_data[i]["seq_data"];
-            var z = sequence_data[i]["subj_data"];
-            graph_data[z]["data"][x]++;
-         }
-         for(i=0;i<graph_data.length;i++)
-         {
-            for(j=0;j<graph_data[i]["data"].length;j++)
-            {
-               graph_data[i]["data"][j]*=equity_factor[i];
-            }
-         }
-         var data = {
-           labels: sequence_info[options["sequence_val"]],
-           series: graph_data
-         };
-         var options = {
-           seriesBarDistance: 10
-         }*
-         new Chartist.Bar('#ct-chart-equity', data, {
-               plugins: [
-                   Chartist.plugins.legend()
-               ]
-           });
-      }
+    var demButtons = $('.dparam-selection');
+    dpairs = dparams['children']['parameterPairs'];
+    for (var d = 0; d < dpairs; d++) {
+      name = dparams['children']['label'+d];
+      id = dparams['children']['label'+d];
 
-      if($("input:checkbox[id=graph_standard]:checked").val()=="on")
-      {
-         $("#graph_container").append("<h3>Standard Graph:</h3>");
-         $("#graph_container").append("<div id='ct-chart-standard' class='ct-chart ct-chart-bar'></div>")
-         graph_standard(options);
-      }
-      if($("input:checkbox[id=graph_demographic]:checked").val()=="on")
-      {
-         $("#graph_container").append("<h3>Demographic Graph:</h3>");
-         $("#graph_container").append("<div id='ct-chart-demographic' class='ct-chart ct-chart-bar'></div>")
-         graph_demographics(options, subjects);
-      }
+      var button = $('<button>', {
+        class: "button is-medium option-selectors",
+        text: name,
+        data_id: id
+      }).appendTo(demButtons);
 
-      if($("input:checkbox[id=graph_equity]:checked").val()=="on")
-      {
-         $("#graph_container").append("<h3>Equity Graph:</h3>");
-         $("#graph_container").append("<div id='ct-chart-equity' class='ct-chart ct-chart-bar'></div>")
-         graph_scaled(options, subjects);
-      }
+      button.click(function (e) {
+          e.preventDefault();
+
+          if ( $(this).hasClass('chosen') ){
+            $(this).removeClass('chosen');
+          } else {
+            $(this).addClass('chosen');
+          }
+        });
+    }
+
+    var seqButtons = $('.sparam-selection');
+    spairs = sparams['children']['parameterPairs'];
+    for (var s = 0; s < spairs; s++) {
+      name = sparams['children']['label'+s];
+      id = sparams['children']['label'+s];
+
+      var button = $('<button>', {
+        class: "button is-medium option-selectors",
+        text: name,
+        data_id: id
+      }).appendTo(seqButtons);
+
+      button.click(function (e) {
+          e.preventDefault();
+
+          if ( $(this).hasClass('chosen') ){
+            $(this).removeClass('chosen');
+          } else {
+            $(this).addClass('chosen');
+          }
+        });
+    }
+
    },
-
-   'click #export_CSV_button': function(e) {
+   //This should probably be a modal??
+  'click #export_CSV_button': function(e) {
 
      var selectEnvironment = $('#selectEnvironment').val();
 
@@ -281,3 +214,123 @@ Template.viewData.events({
       }
     }
 });
+
+function makeDemGraphs(env, dparams) {
+  var subs = Subjects.find({"envId": env}).fetch();
+  var data = {}
+  for (p in dparams) {
+    data[dparams[p]] = {}
+    for (s in subs) {
+      var val = subs[s]['info'][dparams[p]]
+      if (val && val in data[dparams[p]]) {
+        data[dparams[p]][val] += 1;
+      } else if (val) {
+        data[dparams[p]][val] = 1;
+      }
+    }
+  }
+  
+  for (key in data){
+    makePieChart(d3.entries(data[key]), key);
+  }
+
+}
+
+function makeContributionGraphs(obsIds, dp, sp) {
+  data = {};
+  for (d in dp) {
+    data[dp[d]] = {};
+  }
+
+  for (id in obsIds) {
+    var seqs = Sequences.find({"obsId" : obsIds[id]}).fetch();
+    for (seq in seqs) {
+      var studId = seqs[seq]['info']['studentId'];
+      var student = Subjects.findOne({"_id": studId});
+      for (s in sp) {
+        var param = sp[s];
+        var value = seqs[seq]['info'][param];
+        if (!value) {continue; }
+        for (d in dp) {
+          var dem = dp[d];
+          var demVal = student['info'][dem]
+          if (!demVal) {continue; }
+          if (!(param in data[dp[d]])) { data[dem][param] = {}; }
+          if (!(value in data[dem][param])) { data[dem][param][value] = {}; }
+          if (demVal in data[dem][param][value]) {
+            data[dem][param][value][demVal] += 1;
+          } else {
+            data[dem][param][value][demVal] = 1;
+          }
+        }
+
+      }
+    }
+  }
+
+  for (key in data) {
+    for (param in data[key]) {
+      var label = ""+param+" by "+key;
+      var dataSlice = d3.entries(data[key][param]);
+      if (param == "Wait Time"){
+        console.log(label);
+        console.log(dataSlice);
+      }
+    }
+  }
+}
+
+function makePieChart(data, label) {
+
+  var margins = 100, 
+  width = 600 - margins, 
+  height = 500 - margins,
+  radius = Math.min(width, height) / 2;
+
+  var svg = d3.select(".demo-plots")
+            .append("svg")
+            .attr('width', width)
+            .attr('height', height);
+
+  g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  var color = d3.scaleOrdinal()
+    .range(["#F15854", "#DECF3F", "#B276B2", "#B2912F", "#F17CB0", "#60BD68", "#FAA43A"]);
+
+  var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.value; });
+
+  var path = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+  var label = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 70);
+
+  var arc = g.selectAll('.arc')
+      .data(pie(data))
+      .enter().append("g")
+        .attr('class', 'arc');
+
+  arc.append("path")
+    .attr('d', path)
+    .attr('fill', function (d) { return color(d.data.key); });
+
+  arc.append("text")
+      .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+      .attr("dy", "0.35em")
+      .text(function(d) { return d.data.key; });
+
+  svg.append("text")
+    .attr("transform", "translate("+width/2+","+(height-40)+")")
+    .attr("font-size", "40px")
+    .attr("text-anchor", "middle")
+    .text(key);
+
+}
+
+function makeStackedBar(data, label) {
+  
+}
