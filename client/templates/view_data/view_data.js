75b0836610e794715d05bb700f87184e8933fe42
steve-chen-nyc <steve.l.chen@gmail.com>
2017-11-19 01:18:12 -0500

    removed un-needed wrap function

--- client/templates/view_data/view_data.js
+++ client/templates/view_data/view_data.js
@@ -562,8 +562,6 @@ function makeIndividualGraphs(oIds) {
       .attr("width", 70)
       .attr("height", function(d) { return height - y(d.value); })
 
-
-
   g.append("text")
       .attr("x", 100)
       .attr("y", -20)
@@ -571,51 +569,6 @@ function makeIndividualGraphs(oIds) {
       .attr("font-weight", "bold")
       .attr("text-anchor", "middle")
       .text("Contributions over Students");
-
-
-      function wrap (text, width) {
-
-        text.each(function() {
-
-          var breakChars = ['/', '&', '-'],
-            text = d3.select(this),
-            textContent = text.text(),
-            spanContent;
-
-          breakChars.forEach(char => {
-            // Add a space after each break char for the function to use to determine line breaks
-            textContent = textContent.replace(char, char + ' ');
-          });
-
-          var words = textContent.split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            x = text.attr('x'),
-            y = text.attr('y'),
-            dy = parseFloat(text.attr('dy') || 0),
-            tspan = text.text(null).append('tspan').attr('x', x).attr('y', y).attr('dy', dy + 'em');
-
-          while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(' '));
-            if (tspan.node().getComputedTextLength() > width) {
-              line.pop();
-              spanContent = line.join(' ');
-              breakChars.forEach(char => {
-                // Remove spaces trailing breakChars that were added above
-                spanContent = spanContent.replace(char + ' ', char);
-              });
-              tspan.text(spanContent);
-              line = [word];
-              tspan = text.append('tspan').attr('x', x).attr('y', y).attr('dy', ++lineNumber * lineHeight + dy + 'em').text(word);
-            }
-          }
-        });
-
-      }
-
 }
 
 function makePieChart(data, label) {
