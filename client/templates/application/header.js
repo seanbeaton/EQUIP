170c4905d617351dc6e50d80822f94b91140e044
steve-chen-nyc <steve.l.chen@gmail.com>
2018-02-04 23:03:24 -0500

    first round of nav updates

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -2,6 +2,18 @@
 * JS file for header.html
 */
 
-Template.header.helpers({
-  
+Template.header.events({
+     'click #signOut': function(e){
+         Meteor.logout(function(){
+             console.log("user logged out");
+         });
+     },
+     'mouseenter .dropdown-wrapper': function(e) {
+         document.querySelector('.dropdown-wrapper .fa-caret-down').classList.add('hide');
+         document.querySelector('.dropdown-wrapper .fa-caret-up').classList.remove('hide');
+     },
+     'mouseleave .dropdown-wrapper': function(e) {
+         document.querySelector('.dropdown-wrapper .fa-caret-up').classList.add('hide');
+         document.querySelector('.dropdown-wrapper .fa-caret-down').classList.remove('hide');
+     }
 });
