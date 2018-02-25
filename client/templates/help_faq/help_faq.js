2055fe2987149c0c717472174a99ca0f4ce48e2e
steve-chen-nyc <steve.l.chen@gmail.com>
2018-02-24 21:55:01 -0500

    created accordions for help & faq section

---
+++ client/templates/help_faq/help_faq.js
@@ -0,0 +1,25 @@
+Template.helpFaq.helpers({
+  accordion: function() {
+      return [
+          {header: "ONE", copy: "accordion one content"},
+          {header: "TWO", copy: "accordion two content"},
+          {header: "THREE", copy: "accordion three content"},
+          {header: "FOUR", copy: "accordion four content"},
+          {header: "FIVE", copy: "accordion five content"}
+      ]
+  }
+});
+
+Template.helpFaq.events({
+  'click .toggle-accordion': function(e) {
+      e.preventDefault();
+      var ele = e.target;
+
+      if (ele.nodeName === "H2") {
+          ele = ele.parentElement;
+      }
+
+      $(ele).next().toggleClass('show');
+      $(ele).next().slideToggle(350);
+    }
+});
