807fb5d0807b7372dd7fa7d7fce655405f372553
seanbeaton <beatonseanp@gmail.com>
2018-11-29 20:49:34 +0100

    update copy for seq params

--- client/templates/parameters/edit_sequence_parameters.js
+++ client/templates/parameters/edit_sequence_parameters.js
@@ -259,7 +259,7 @@ const EditSequence = () => {
             },
             {
                 name: "Teacher Solicitation",
-                input: "How,What,Why,Other,N/A"
+                input: "How,What,Why,Other,None"
             },
             {
                 name: "Wait Time",
