69d99921580dc7d9844efe134f8bd7b1ff3b5e4c
seanbeaton <beatonseanp@gmail.com>
2018-11-29 22:18:59 +0100

    remove dupe code from main js

--- client/main.js
+++ client/main.js
@@ -1,19 +1,3 @@
 /*
 * Top level JS file
 */
-
-Template.body.events({
-  'mouseup': function(e) {
-    let menu_wrapper = $('.dropdown-wrapper');
-    console.log('clickup', e.target, menu_wrapper.has(e.target));
-    if ( !menu_wrapper.is(e.target) && menu_wrapper.has(e.target).length === 0) {
-      menu_wrapper.removeClass('dropdown-wrapper--open');
-      console.log('vdalid');
-    }
-  },
-
-
-  'click .dropdown-wrapper': function(e) {
-    $('.dropdown-wrapper').toggleClass('dropdown-wrapper--open')
-  },
-})
