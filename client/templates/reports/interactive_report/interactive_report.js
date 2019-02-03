3156545a1fd808982bcaf69b69ea73319e8ab41d
seanbeaton <beatonseanp@gmail.com>
2019-02-03 21:05:08 +0100

    use viridis colors

--- client/templates/reports/interactive_report/interactive_report.js
+++ client/templates/reports/interactive_report/interactive_report.js
@@ -1068,14 +1068,30 @@ let available_colors = [
 ];
 
 
+let avail_colors_viridis = [
+  "#440154ff", "#440558ff", "#450a5cff", "#450e60ff", "#451465ff", "#461969ff",
+  "#461d6dff", "#462372ff", "#472775ff", "#472c7aff", "#46307cff", "#45337dff",
+  "#433880ff", "#423c81ff", "#404184ff", "#3f4686ff", "#3d4a88ff", "#3c4f8aff",
+  "#3b518bff", "#39558bff", "#37598cff", "#365c8cff", "#34608cff", "#33638dff",
+  "#31678dff", "#2f6b8dff", "#2d6e8eff", "#2c718eff", "#2b748eff", "#29788eff",
+  "#287c8eff", "#277f8eff", "#25848dff", "#24878dff", "#238b8dff", "#218f8dff",
+  "#21918dff", "#22958bff", "#23988aff", "#239b89ff", "#249f87ff", "#25a186ff",
+  "#25a584ff", "#26a883ff", "#27ab82ff", "#29ae80ff", "#2eb17dff", "#35b479ff",
+  "#3cb875ff", "#42bb72ff", "#49be6eff", "#4ec16bff", "#55c467ff", "#5cc863ff",
+  "#61c960ff", "#6bcc5aff", "#72ce55ff", "#7cd04fff", "#85d349ff", "#8dd544ff",
+  "#97d73eff", "#9ed93aff", "#a8db34ff", "#b0dd31ff", "#b8de30ff", "#c3df2eff",
+  "#cbe02dff", "#d6e22bff", "#e1e329ff", "#eae428ff", "#f5e626ff", "#fde725ff",
+];
+
+
 function shiftArrayToLeft(arr, places) {
   arr.push.apply(arr, arr.splice(0,places));
 }
 
 let getLabelColors = function(labels) {
-  let local_colors = available_colors.slice();
+  let local_colors = avail_colors_viridis.slice();
 
-  let spacing = Math.max(Math.floor(available_colors.length / labels.length), 1);
+  let spacing = Math.max(Math.floor(avail_colors_viridis.length / labels.length), 1);
 
   let label_colors = {};
   let _ = labels.map(function(label) {
