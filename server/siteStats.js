c69eb8207ae57aba220271846cbdd9d05dbee65b
seanbeaton <sean@seanbeaton.com>
2020-06-02 20:45:01 +0200

    reenable site stats

--- server/siteStats.js
+++ server/siteStats.js
@@ -5,7 +5,6 @@ CachedStats = new Mongo.Collection('cached_stats');
 
 const total_color = "#F25F3D";
 const month_color = "#999999";
-let d3 = require('d3');
 
 
 Meteor.methods({
@@ -27,11 +26,9 @@ Meteor.methods({
       sort: { createdAt : -1 }
     });
 
-    console.log('cached stats', stats);
-
     if (!stats) {
-      let seq_params = SequenceParameters.find().fetch();
-      let subj_params = SubjectParameters.find().fetch();
+      let seq_params = SequenceParameters.find({}, {reactive: false}).fetch();
+      let subj_params = SubjectParameters.find({}, {reactive: false}).fetch();
       stats = {
         sequences_pop: _getParamPopularity(seq_params),
         demographics_pop: _getParamPopularity(subj_params),
@@ -61,16 +58,15 @@ Meteor.methods({
       sort: { createdAt : -1 }
     });
 
-    console.log('cached stats', stats);
 
     if (!stats) {
       let items = []
-      items.push({label: 'Number of users', value: Meteor.users.find().count()});
-      items.push({label: 'Number of classrooms', value: Environments.find({isExample: null, envName: {$ne: "Example Classroom"}}).count()});
-      items.push({label: 'Number of example classrooms', value: Environments.find({$or: [{isExample: true}, {envName: "Example Classroom"}]}).count()});
-      items.push({label: 'Number of students', value: Subjects.find({origStudId: null}).count()});
-      items.push({label: 'Number of observations', value: Observations.find({origObsId: null}).count()});
-      items.push({label: 'Number of sequences', value: Sequences.find({origObsId: null}).count()});
+      items.push({label: 'Number of users', value: Meteor.users.find({}, {reactive: false}).count()});
+      items.push({label: 'Number of classrooms', value: Environments.find({isExample: null, envName: {$ne: "Example Classroom"}}, {reactive: false}).count()});
+      items.push({label: 'Number of example classrooms', value: Environments.find({$or: [{isExample: true}, {envName: "Example Classroom"}]}, {reactive: false}).count()});
+      items.push({label: 'Number of students', value: Subjects.find({origStudId: null}, {reactive: false}).count()});
+      items.push({label: 'Number of observations', value: Observations.find({origObsId: null}).count()}, {reactive: false});
+      items.push({label: 'Number of sequences', value: Sequences.find({origObsId: null}).count()}, {reactive: false});
 
       stats = {
         stats: items,
@@ -82,7 +78,7 @@ Meteor.methods({
     }
     return stats
   },
-  getStatsTime: function(refresh) {
+  getStatsTime: async function(refresh) {
     if (typeof refresh === 'undefined') {
       refresh = false;
     }
@@ -100,15 +96,13 @@ Meteor.methods({
       sort: { createdAt : -1 }
     });
 
-    console.log('cached stats', stats);
-
     if (!stats) {
       let items = []
-      items.push({selector: 'new_users_graph', group_label_prefix: new_total_guide(), group_label: 'users', months: users_over_time()});
-      items.push({selector: 'new_classrooms_graph', group_label_prefix: new_total_guide(), group_label: 'classrooms', months: classrooms_over_time()});
-      items.push({selector: 'new_students_graph', group_label_prefix: new_total_guide(), group_label: 'students', months: students_over_time()});
-      items.push({selector: 'new_observations_graph', group_label_prefix: new_total_guide(), group_label: 'observations', months: observations_over_time()});
-      items.push({selector: 'new_sequences_graph', group_label_prefix: new_total_guide(), group_label: 'sequences', months: sequences_over_time()});
+      items.push({selector: 'new_classrooms_graph', group_label_prefix: new_total_guide(), group_label: 'classrooms', months: await classrooms_over_time()});
+      items.push({selector: 'new_users_graph', group_label_prefix: new_total_guide(), group_label: 'users', months: await users_over_time()});
+      items.push({selector: 'new_students_graph', group_label_prefix: new_total_guide(), group_label: 'students', months: await students_over_time()});
+      items.push({selector: 'new_observations_graph', group_label_prefix: new_total_guide(), group_label: 'observations', months: await observations_over_time()});
+      items.push({selector: 'new_sequences_graph', group_label_prefix: new_total_guide(), group_label: 'sequences', months: await sequences_over_time()});
 
       stats = {
         stats: items,
@@ -133,7 +127,6 @@ function _getParamPopularity(parameter_sets) {
   let labels = {}
   for (let pk in parameter_sets) {
     let parameters = parameter_sets[pk]['children'];
-    // console.log('parameters')
     for (let i = 0; i < parameters.parameterPairs; i++) {
       if (typeof labels[parameters[`label${i}`]] !== 'undefined') {
         labels[parameters[`label${i}`]]['count']++
@@ -181,60 +174,70 @@ function _getParamPopularity(parameter_sets) {
 
 
 let users_over_time = function() {
-  return get_grouped_data(Meteor.users.find().fetch(), 'createdAt')
+  let search = {};
+  let collection = Meteor.users;
+  let result = get_grouped_data(search, collection, '$createdAt')
+  return result;
 }
 
 let classrooms_over_time = function() {
-  return get_grouped_data(Environments.find({isExample: null, envName: {$ne: "Example Classroom"}}).fetch(), 'submitted')
+  let search = {isExample: null, envName: {$ne: "Example Classroom"}};
+  let collection = Environments;
+  let result = get_grouped_data(search, collection, '$submitted')
+  return result;
 }
 
 let students_over_time = function() {
-  return get_grouped_data(Subjects.find({origStudId: null}).fetch(), 'submitted')
+  let search = {origStudId: null};
+  let collection = Subjects;
+  let result = get_grouped_data(search, collection, '$submitted')
+  return result;
 }
 
 let observations_over_time = function() {
-  return get_grouped_data(Observations.find({origObsId: null}).fetch(), 'submitted')
+  let search = {origObsId: null};
+  let collection = Observations;
+  let result = get_grouped_data(search, collection, '$submitted')
+  return result;
 }
 
 let sequences_over_time = function() {
-  return get_grouped_data(Sequences.find({origObsId: null}).fetch(), 'submitted')
+  let search = {origObsId: null};
+  let collection = Sequences;
+  let result = get_grouped_data(search, collection, '$submitted')
+  return result;
 }
 
 
-let get_grouped_data = function(data, key) {
-  let month_groups = group_by(data, key);
-  let stats = [];
-  Object.keys(month_groups).forEach(function(month_key) {
+let get_grouped_data = async function(search, collection, key) {
+  let d3 = require('d3');
+  const command = [{
+    $match: search
+  }, {
+    $group: {
+      _id : { year: { $year: key } , month: { $month: key }},
+      count: { $sum: 1 }
+    }
+  }];
+  let res = collection.rawCollection().aggregate(command);
+  const locale = 'en-us';
+  let stats = []
+  await res.forEach(function(date_count) {
+    let human_month = new Date(`${date_count._id.year}-${date_count._id.month.toString().padStart(2, '0')}-01`).toLocaleString(locale, {timeZone: 'UTC', month: 'long', year: 'numeric'});
     stats.push({
-      label: month_key,
-      value: month_groups[month_key].length,
-      d3date: d3.timeParse('%B %Y')(month_key)
+      label: human_month,
+      value: date_count.count,
+      d3date: d3.timeParse('%B %Y')(human_month)
     })
   })
   stats.sort(function(a, b) {
-    // console.log('a, b', a , b);
     return (new Date(a.label) - new Date(b.label))
-  })
-
+  });
   let running_total = 0;
   stats.forEach(function(stat) {
     running_total += stat.value;
     stat.total = running_total;
-  })
+  });
 
   return stats;
-};
-
-let group_by = function(xs, key) {
-  let locale = "en-us";
-  return xs.reduce(function(rv, x) {
-    let date_month;
-    if (!x[key]) {
-      date_month = 'Not recorded';
-    } else {
-      date_month = x[key].toLocaleString(locale, {month: 'long', year: 'numeric'});
-    }
-    (rv[date_month] = rv[date_month] || []).push(x);
-    return rv;
-  }, {});
 };
