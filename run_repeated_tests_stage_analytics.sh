1151f72a4282ecfb959e6efacc9bdca782a1aa0b
seanbeaton <sean@seanbeaton.com>
2020-07-14 13:11:16 +0200

    hide example classroom creation option

---
+++ run_repeated_tests_stage_analytics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/sean/app/sbc/dataObs
+
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
+npm run chimp-stress-stage;
