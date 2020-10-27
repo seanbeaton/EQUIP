650248d29213c0c620ec1dfe48097ae5a1532285
seanbeaton <sean@seanbeaton.com>
2020-10-27 19:51:39 +0100

    group research by researchCategorization

--- client/templates/research/research.js
+++ client/templates/research/research.js
@@ -1,7 +1,24 @@
 Template.research.helpers({
-  researchContent: function () {
-    return ResearchContent.find({'published': true}, {
+  researchContentCategorized: function () {
+    let content = ResearchContent.find({'published': true}, {
       'sort': [['researchDateSort', 'desc'], ['weight', 'asc']]
     }).fetch();
+
+    let categories = [];
+    content.forEach(function(ct) {
+      let cat_index = categories.findIndex(cat => cat.category_name === ct.researchCategorization)
+      if (cat_index === -1) {
+        categories.push({
+          'category_name': ct.researchCategorization,
+          'children': [ct],
+        })
+      }
+      else {
+        categories[cat_index].children.push(ct)
+      }
+    })
+
+    console.log('content', content);
+    return categories.sort((a, b) => b.category_name < a.category_name);
   },
 });
