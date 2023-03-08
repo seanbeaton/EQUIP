c3bea67d36d3e87e0dc7467f8bca50fab02c5d39
seanbeaton <sean@seanbeaton.com>
2023-03-08 16:40:13 +0100

    use new color scheme

--- client/templates/reports/interactive_report_new/interactive_report_new.js
+++ client/templates/reports/interactive_report_new/interactive_report_new.js
@@ -255,14 +255,14 @@ Template.interactiveReportNew.onCreated(function created() {
   this.updateKey = (key_wrapper) => {
     var d3 = require('d3');
     let y_axis = this.getYAxisSelection();
-    let color_function;
+    let colors_reverse;
     if (y_axis.param_type === 'demographics') {
-      color_function = d3.interpolateViridis
+      colors_reverse = false
     }
     else {
-      color_function = d3.interpolatePlasma
+      colors_reverse = true
     }
-    let label_colors = this.getLabelColors(y_axis.selected_option.option_list, color_function);
+    let label_colors = this.getLabelColors(y_axis.selected_option.option_list, colors_reverse);
     let key_chunks = Object.keys(label_colors).map(function (label) {
       let color = label_colors[label]
       return `<span class="key--label"><span class="key--color" style="background-color: ${color}"></span><span class="key--text">${label}</span></span>`
@@ -321,15 +321,17 @@ Template.interactiveReportNew.onCreated(function created() {
     var keys = contribData.column_keys;
 
 
-    let color_function;
+    let colors_reverse;
     console_log_conditional('contribData', contribData);
     if (contribData.y_axis_param_type === 'demographics') {
-      color_function = d3.interpolateViridis
+      colors_reverse = false
     }
     else {
-      color_function = d3.interpolatePlasma
+      colors_reverse = true
     }
-    let key_colors = this.getLabelColors(keys, color_function);
+    console.log('colors_reverse', colors_reverse);
+    let key_colors = this.getLabelColors(keys, colors_reverse);
+    console.log('createGraph key_colors', key_colors)
     var z = d3.scaleOrdinal()
       .range(Object.values(key_colors));
 
@@ -685,29 +687,18 @@ let buildBarTooltipSlide = function (group, group_type, bar, bar_type, contribDa
 
 
 
-let available_colors = [
-  "#003f5c",
-  "#2f4b7c",
-  "#665191",
-  "#a05195",
-  "#d45087",
-  "#f95d6a",
-  "#ff7c43",
-  "#ffa600"
-];
-
-
-let avail_colors_viridis = [
-  "#440154ff", "#440558ff", "#450a5cff", "#450e60ff", "#451465ff", "#461969ff",
-  "#461d6dff", "#462372ff", "#472775ff", "#472c7aff", "#46307cff", "#45337dff",
-  "#433880ff", "#423c81ff", "#404184ff", "#3f4686ff", "#3d4a88ff", "#3c4f8aff",
-  "#3b518bff", "#39558bff", "#37598cff", "#365c8cff", "#34608cff", "#33638dff",
-  "#31678dff", "#2f6b8dff", "#2d6e8eff", "#2c718eff", "#2b748eff", "#29788eff",
-  "#287c8eff", "#277f8eff", "#25848dff", "#24878dff", "#238b8dff", "#218f8dff",
-  "#21918dff", "#22958bff", "#23988aff", "#239b89ff", "#249f87ff", "#25a186ff",
-  "#25a584ff", "#26a883ff", "#27ab82ff", "#29ae80ff", "#2eb17dff", "#35b479ff",
-  "#3cb875ff", "#42bb72ff", "#49be6eff", "#4ec16bff", "#55c467ff", "#5cc863ff",
-  "#61c960ff", "#6bcc5aff", "#72ce55ff", "#7cd04fff", "#85d349ff", "#8dd544ff",
-  "#97d73eff", "#9ed93aff", "#a8db34ff", "#b0dd31ff", "#b8de30ff", "#c3df2eff",
-  "#cbe02dff", "#d6e22bff", "#e1e329ff", "#eae428ff", "#f5e626ff", "#fde725ff",
-];
+//
+// let avail_colors_viridis = [
+//   "#440154ff", "#440558ff", "#450a5cff", "#450e60ff", "#451465ff", "#461969ff",
+//   "#461d6dff", "#462372ff", "#472775ff", "#472c7aff", "#46307cff", "#45337dff",
+//   "#433880ff", "#423c81ff", "#404184ff", "#3f4686ff", "#3d4a88ff", "#3c4f8aff",
+//   "#3b518bff", "#39558bff", "#37598cff", "#365c8cff", "#34608cff", "#33638dff",
+//   "#31678dff", "#2f6b8dff", "#2d6e8eff", "#2c718eff", "#2b748eff", "#29788eff",
+//   "#287c8eff", "#277f8eff", "#25848dff", "#24878dff", "#238b8dff", "#218f8dff",
+//   "#21918dff", "#22958bff", "#23988aff", "#239b89ff", "#249f87ff", "#25a186ff",
+//   "#25a584ff", "#26a883ff", "#27ab82ff", "#29ae80ff", "#2eb17dff", "#35b479ff",
+//   "#3cb875ff", "#42bb72ff", "#49be6eff", "#4ec16bff", "#55c467ff", "#5cc863ff",
+//   "#61c960ff", "#6bcc5aff", "#72ce55ff", "#7cd04fff", "#85d349ff", "#8dd544ff",
+//   "#97d73eff", "#9ed93aff", "#a8db34ff", "#b0dd31ff", "#b8de30ff", "#c3df2eff",
+//   "#cbe02dff", "#d6e22bff", "#e1e329ff", "#eae428ff", "#f5e626ff", "#fde725ff",
+// ];
--- helpers/graphsv2.js
+++ helpers/graphsv2.js
@@ -21,18 +21,29 @@ export const xor = function (a, b) {
   return (a || b) && !(a && b);
 };
 
-export const getLabelColors = function (labels, color_function) {
-  var d3 = require('d3');
-  if (typeof color_function === 'undefined') {
-    color_function = d3.interpolateViridis;
+export const getLabelColors = function (labels, reverse_colors, colors) {
+  // var d3 = require('d3');
+  if (typeof colors === 'undefined') {
+    colors = available_colors;
   }
 
-  let spacing = 1 / labels.length;
+  colors = JSON.parse(JSON.stringify(colors));
+
+  if (typeof reverse_colors === 'undefined') {
+    reverse_colors = false;
+  }
+
+  if (reverse_colors) {
+    colors.reverse();
+  }
+
+  console.log()
 
   let label_colors = {};
   let _ = labels.map(function (label, index) {
     if (typeof label_colors[label] === 'undefined') {
-      label_colors[label] = color_function(index * spacing);
+      // console.log('index, available_colors.length - 1, (available_colors.length - 1) % index', index, colors.length - 1, index % (available_colors.length - 1))
+      label_colors[label] = colors[index % (colors.length - 1)];
     }
     else {
 
@@ -40,3 +51,10 @@ export const getLabelColors = function (labels, color_function) {
   });
   return label_colors
 }
+
+let available_colors = [
+  '#77AADD', '#EE8866', '#EEDD88', '#FFAABB',
+  '#99DDFF', '#44BB99', '#BBCC33', '#AAAA00',
+  '#DDDDDD', '#555555', '#222255', '#225555',
+  '#225522', '#666633', '#663333',
+];
